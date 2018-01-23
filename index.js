f216a1fa4012c4911faae2382c5dbfc430543d0f
Brandon <31463964+BrandonJMorgan@users.noreply.github.com>
2018-01-22 23:27:01 -0600

    Added contact form for firebase

---
+++ index.js
@@ -0,0 +1,55 @@
+// Initialize Firebase (ADD YOUR OWN DATA)
+var config = {
+   apiKey: "AIzaSyC8UDTbEG5bpbGwdPAeuxPKEG8mt1YG7w0",
+    authDomain: "contactform-fce78.firebaseapp.com",
+    databaseURL: "https://contactform-fce78.firebaseio.com",
+    projectId: "contactform-fce78",
+    storageBucket: "contactform-fce78.appspot.com",
+    messagingSenderId: "313988068172"
+};
+firebase.initializeApp(config);
+
+// Reference messages collection
+var messagesRef = firebase.database().ref('messages');
+
+// Listen for form submit
+document.getElementById('contactForm').addEventListener('submit', submitForm);
+
+// Submit form
+function submitForm(e){
+  e.preventDefault();
+
+  // Get values
+  var name = getInputVal('name');
+  var email = getInputVal('email');
+  var message = getInputVal('message');
+
+  // Save message
+  saveMessage(name, company, email, phone, message);
+
+  // Show alert
+  document.querySelector('.alert').style.display = 'block';
+
+  // Hide alert after 3 seconds
+  setTimeout(function(){
+    document.querySelector('.alert').style.display = 'none';
+  },3000);
+
+  // Clear form
+  document.getElementById('contactForm').reset();
+}
+
+// Function to get get form values
+function getInputVal(id){
+  return document.getElementById(id).value;
+}
+
+// Save message to firebase
+function saveMessage(name, company, email, phone, message){
+  var newMessageRef = messagesRef.push();
+  newMessageRef.set({
+    name: name,
+    email:email,
+    message:message
+  });
+}
